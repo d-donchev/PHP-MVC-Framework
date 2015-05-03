b40af7efddd62f657e4f4a74ebeca02a126d7abc
d-donchev <ddonchev@gmail.com>
2015-05-03 23:46:39 +0300

    Small Fix

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ session_start();
 
 require_once('includes/config.php');
 
+$requestParts = explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
+var_dump($requestParts); //1:47
 
 function __autoload($class_name) {
     if (file_exists("controllers/$class_name.php")) {
