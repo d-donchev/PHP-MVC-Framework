5ebeee1501807a391085a52de7580e8890aa563d
d-donchev <ddonchev@gmail.com>
2015-05-03 23:17:44 +0300

    Full structure

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
-var_dump($_SERVER);
+session_start();
+
+require_once('includes/config.php');
+
+
+
+function __autoload($class_name) {
+    if (file_exists("controllers/$class_name.php")) {
+        include "controllers/$class_name.php";
+    }
+    if (file_exists("models/$class_name.php")) {
+        include "models/$class_name.php";
+    }
+}
