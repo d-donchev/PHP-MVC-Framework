d5ff949bc179907bd2ebc1a0e4e0eee89688a575
d-donchev <ddonchev@gmail.com>
2015-05-04 23:08:41 +0300

    BaseController

--- includes/config.php
+++ includes/config.php
@@ -7,4 +7,4 @@ define('DEFAULT_LAYOUT', 'default');
 define('DB_HOST', 'localhost:81/PHP-MVC-Framework');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mvc');
+define('DB_NAME', 'mvc_fw');
